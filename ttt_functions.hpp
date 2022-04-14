ad74d7c225f57049a8c8ca758a6feca6046890e5
Josue Medina <38742303+chekel321@users.noreply.github.com>
2022-04-14 00:57:14 -0400

    First Commit

---
+++ ttt_functions.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// Declarations
+void introduction();
+bool is_winner();
+bool filled_up();
+void draw_board();
+void set_position();
+void update_board();
+void change_player();
+void take_turn();
+void end_game();
